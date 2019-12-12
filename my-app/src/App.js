e1a0323de9a9f21c051af419492b523520229027
imxande <alerodriguezlarosa@gmail.com>
2019-12-12 16:52:08 -0500

    initial commit - setting up project with git

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Im ready
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/action/index.js
@@ -0,0 +1,6 @@
+import {
+    FETCH_DATA_START,
+    FETCH_DATA_SUCCESS,
+    FETCH_DATA_FAILURE
+  } from '../actions';
+  
